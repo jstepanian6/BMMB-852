9a03564f2396bcd104b42e46069ffc3148a81174
Johanna Stepanian <137444188+jstepanian6@users.noreply.github.com>
2025-09-25 07:50:52 -0400

    Update get_fastq.sh

--- week5/get_fastq.sh
+++ week5/get_fastq.sh
@@ -5,11 +5,10 @@ SRR=$1
 READ_DIR=reads
 
 #--------- NO CHANGES BELOW THIS LINE --------
+
 mkdir -p $READ_DIR
 
-
-##TODO Fix the X to get 10x coverage -X number of reads 
-fastq-dump -X 137931 -F --outdir reads --split-files $1
+fastq-dump -X 137931 -F --outdir $READ_DIR --split-files $1 
 
 #Stats 
 seqkit stats $READ_DIR/${SRR}_1.fastq $READ_DIR/${SRR}_2.fastq
